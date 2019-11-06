790dc7c42556efcf3b8b8a09b82e12814b69e121
kj_cho <kj_cho@idealidea.co.kr>
2019-11-06 21:30:42 +0900

    DataClass를 통째로 첨부.

--- app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/datas/NoticeData.kt
+++ app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/datas/NoticeData.kt
@@ -1,8 +1,9 @@
 package com.tjoeun.a20191106_01_listviewpractice.datas
 
 import android.util.Log
+import java.io.Serializable
 
-class NoticeData(title:String, content:String, writeDate:String) {
+class NoticeData(title:String, content:String, writeDate:String) : Serializable {
 
     var title = title
     var content = content
