52020113d9d4d9a379fb6a9cd0a61886ba5c2515
kj_cho <kj_cho@idealidea.co.kr>
2019-11-06 19:56:03 +0900

    데이터클래스 생성

---
+++ app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/datas/NoticeData.kt
@@ -0,0 +1,15 @@
+package com.tjoeun.a20191106_01_listviewpractice.datas
+
+import android.util.Log
+
+class NoticeData(title:String, content:String, writeDate:String) {
+
+    var title = title
+    var content = content
+    var date = writeDate
+
+//    init {
+//        Log.d("제목", title)
+//    }
+
+}
